7108704992715c6612cc6f5909e5944fe53e7cf0
erijben <129642847+erijben@users.noreply.github.com>
2024-05-17 21:09:20 +0100

    hhhhjjjhhhgg

--- src/authContexte/AuthContext.js
+++ src/authContexte/AuthContext.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 import { createContext, useContext,useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
@@ -74,4 +74,4 @@ useEffect(() => {
 
 export function useAuth() {
   return useContext(AuthContext);
-}
+}*/
