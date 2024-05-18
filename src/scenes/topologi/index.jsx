6c71400a2c49b8ecae9918dcf88fc2e07cbc46ec
erijben <129642847+erijben@users.noreply.github.com>
2024-05-18 13:54:32 +0100

    aya ekhdem aadhhghhjhhhhhjghhghgghhghggggggggghjjf

--- src/scenes/topologi/index.jsx
+++ src/scenes/topologi/index.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
-import { Box, Button, Typography, Paper } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Typography, Snackbar, Paper } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Graph from 'react-graph-vis';
+import 'vis-network/styles/vis-network.css';
 
 const Topologi = () => {
   const navigate = useNavigate();
   const [scannedEquipments, setScannedEquipments] = useState([]);
-  const [selectedEquipments, setSelectedEquipments] = useState([]);
+  const [equipmentList, setEquipmentList] = useState([]);
+  const [graph, setGraph] = useState({ nodes: [], edges: [] });
+  const [selectedEquipmentId, setSelectedEquipmentId] = useState(null);
+  const [alertMessage, setAlertMessage] = useState('');
+
+  useEffect(() => {
+    const fetchEquipments = async () => {
+      try {
+        const response = await axios.get('https://nodeapp-ectt.onrender.com/equip');
+        setEquipmentList(response.data);
+      } catch (error) {
+        console.error('Error fetching equipments:', error);
+      }
+    };
+    fetchEquipments();
+  }, []);
 
   const handleRFIDScan = async () => {
     try {
@@ -14,18 +31,32 @@ const Topologi = () => {
       await ndef.scan();
       ndef.addEventListener('reading', async event => {
         const rfid = event.serialNumber;
-        console.log('RFID scanned:', rfid); // Debug: Log the scanned RFID
-        try {
-          const response = await axios.get(`https://nodeapp-ectt.onrender.com/find/${rfid}`);
-          console.log('Response from server:', response.data); // Debug: Log the response
-          if (response.data.success) {
-            setScannedEquipments(prevState => [...prevState, response.data.equipment]);
-            console.log('Scanned Equipments:', scannedEquipments); // Debug: Log the scanned equipments
-          } else {
-            console.error('Équipement non trouvé');
+        console.log('RFID scanned:', rfid);
+        const scannedEquipment = equipmentList.find(equip => equip.RFID === rfid);
+        if (scannedEquipment) {
+          let newScannedEquipments = [...scannedEquipments];
+          if (selectedEquipmentId) {
+            const selectedEquipment = newScannedEquipments.find(equip => equip._id === selectedEquipmentId);
+            if (selectedEquipment) {
+              selectedEquipment.ConnecteA.push(scannedEquipment._id);
+              try {
+                await axios.put(`https://nodeapp-ectt.onrender.com/equip/equip/${selectedEquipment._id}`, selectedEquipment);
+                setAlertMessage(`Connexion créée entre ${selectedEquipment.Nom} et ${scannedEquipment.Nom}`);
+              } catch (updateError) {
+                console.error('Error updating equipment:', updateError);
+              }
+            }
           }
-        } catch (error) {
-          console.error('Erreur lors de la recherche de l\'équipement :', error);
+          if (!newScannedEquipments.find(equip => equip._id === scannedEquipment._id)) {
+            newScannedEquipments = [...newScannedEquipments, scannedEquipment];
+          }
+          setScannedEquipments(newScannedEquipments);
+          setSelectedEquipmentId(null);
+          updateGraph(newScannedEquipments);
+          await axios.post('https://nodeapp-ectt.onrender.com/scannedEquipments', newScannedEquipments);
+        } else {
+          setAlertMessage('Équipement non trouvé');
+          console.error('Équipement non trouvé');
         }
       });
     } catch (error) {
@@ -33,77 +64,114 @@ const Topologi = () => {
     }
   };
 
-  const handleSelectEquipment = (equip) => {
-    if (selectedEquipments.includes(equip)) {
-      setSelectedEquipments(prevState => prevState.filter(e => e._id !== equip._id));
-    } else {
-      setSelectedEquipments(prevState => [...prevState, equip]);
-    }
-  };
-
-  const handleCreateConnection = () => {
-    if (selectedEquipments.length === 2) {
-      const [from, to] = selectedEquipments.map(equip => equip._id);
-      axios.post('https://nodeapp-ectt.onrender.com/connections', { from, to })
-        .then(response => {
-          if (response.data.success) {
-            console.log('Connection created successfully');
-            setSelectedEquipments([]);
-            updateScannedEquipments(from, to);
-          }
-        })
-        .catch(error => {
-          console.error('Error creating connection:', error);
-        });
-    } else {
-      console.error('Please select exactly 2 equipments to create a connection');
-    }
-  };
-
-  const updateScannedEquipments = (from, to) => {
-    setScannedEquipments(prevState => prevState.map(equip => {
-      if (equip._id === from || equip._id === to) {
-        return { ...equip, ConnecteA: [...equip.ConnecteA, from === equip._id ? to : from] };
-      }
-      return equip;
+  const updateGraph = (equipments) => {
+    const nodes = equipments.map(equip => ({
+      id: equip._id,
+      label: equip.Nom,
+      shape: 'image',
+      image: selectIconBasedOnType(equip.Type),
+      title: `Type: ${equip.Type}\nAdresse IP: ${equip.AdresseIp}\nRFID: ${equip.RFID}\nEtat: ${equip.Etat}`,
+      color: getColorByState(equip.Etat)
     }));
+    const edges = [];
+    equipments.forEach(equip => {
+      equip.ConnecteA.forEach(connectedId => {
+        edges.push({
+          from: equip._id,
+          to: connectedId,
+          arrows: 'to'
+        });
+      });
+    });
+    setGraph({ nodes, edges });
+  };
+
+  const selectIconBasedOnType = (type) => {
+    switch (type) {
+      case 'router':
+        return `${process.env.PUBLIC_URL}/icons/router.png`;
+      case 'switch':
+        return `${process.env.PUBLIC_URL}/icons/switch.png`;
+      case 'computer':
+        return `${process.env.PUBLIC_URL}/icons/computer.png`;
+      default:
+        return `${process.env.PUBLIC_URL}/icons/default.png`;
+    }
+  };
+
+  const getColorByState = (state) => {
+    switch (state) {
+      case 'dysfonctionnel':
+        return 'red';
+      case 'Problème de réseau':
+        return 'orange';
+      case 'En bon état':
+        return 'green';
+      default:
+        return 'blue';
+    }
+  };
+
+  const options = {
+    layout: {
+      hierarchical: false
+    },
+    nodes: {
+      shape: 'image',
+      size: 30,
+      borderWidth: 2,
+      shapeProperties: {
+        useImageSize: false,
+        useBorderWithImage: true
+      }
+    },
+    edges: {
+      color: "#000000",
+      arrows: {
+        to: { enabled: true, scaleFactor: 1 }
+      }
+    },
+    height: "500px",
+    interaction: {
+      selectable: true,
+      selectConnectedEdges: false,
+    },
+    manipulation: {
+      enabled: true,
+      initiallyActive: true,
+    },
+  };
+
+  const events = {
+    selectNode: (event) => {
+      const { nodes } = event;
+      setSelectedEquipmentId(nodes[0]);
+      setAlertMessage(`Équipement sélectionné: ${nodes[0]}`);
+    }
   };
 
   return (
     <Box m="20px">
-      <Typography variant="h3" mb="20px">Topologie</Typography>
+      <Typography variant="h3" mb="20px">Inventaire</Typography>
       <Button variant="contained" color="primary" onClick={handleRFIDScan}>
         Scanner RFID
       </Button>
-      <Box mt="20px" display="flex" flexWrap="wrap">
-        {scannedEquipments.map(equip => (
-          <Paper 
-            key={equip._id} 
-            onClick={() => handleSelectEquipment(equip)} 
-            style={{ 
-              margin: '10px', 
-              padding: '10px', 
-              cursor: 'pointer', 
-              border: selectedEquipments.includes(equip) ? '2px solid blue' : '1px solid gray' 
-            }}
-          >
-            <Typography>Nom : {equip.Nom}</Typography>
-            <Typography>Type : {equip.Type}</Typography>
-            <Typography>Adresse IP : {equip.AdresseIp}</Typography>
-            <Typography>RFID : {equip.RFID}</Typography>
-            <Typography>Connecté à : {equip.ConnecteA.join(', ')}</Typography>
-          </Paper>
-        ))}
+      <Box mt="20px">
+        <Typography variant="h5">Équipements scannés :</Typography>
+        <Graph
+          key={Date.now()}
+          graph={graph}
+          options={options}
+          events={events}
+          style={{ height: "500px" }}
+        />
       </Box>
-      <Button 
-        variant="contained" 
-        color="secondary" 
-        onClick={handleCreateConnection} 
-        mt="20px"
-        disabled={selectedEquipments.length !== 2}
-      >
-        Créer Connexion
-      </Button>
+      <Snackbar
+        open={!!alertMessage}
+        autoHideDuration={6000}
+        onClose={() => setAlertMessage('')}
+        message={alertMessage}
+      />
       <Button variant="contained" color="secondary" onClick={() => navigate('/dashboard')} mt="20px">
         Retour au Dashboard
       </Button>
