ebb8f82fa0edd5e3422b8ee1317d2388dbb91999
erijben <129642847+erijben@users.noreply.github.com>
2024-05-18 11:35:39 +0100

    aya ekhdem aadhhghhjhhhhhjghhg

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ const isForgotPasswordPage = location.pathname === '/forgot' || location.pathnam
                 <Route path="/pie/:equipmentId" element={<Pie />} />
                 <Route path="/user" element={<User/>} />
                 <Route path="/forgot" element={ <ResetPasswordForm/>} />
-                <Route path="/topologi" elment={<Topologi/>}/>
+                <Route path="/Topologi" elment={<Topologi/>}/>
                 <Route path="/password" element={ <ForgotPasswordForm/>} />
                 </Routes>
            </main>
